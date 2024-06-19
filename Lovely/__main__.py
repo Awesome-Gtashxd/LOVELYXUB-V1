db8edf10eb83e7e45b46915b82ae506d713d154a
Anas Gtash <99383665+Awesome-Gtashxd@users.noreply.github.com>
2024-06-19 20:39:46 +0530

    Create __main__.py

---
+++ Lovely/__main__.py
@@ -0,0 +1,21 @@
+import pyrogram
+import strings
+import config
+
+from Lovely import bot, lovely
+from Lovely.helpers.help_func import get_datetime 
+
+async def run_clients():
+      await bot.start()
+      await lovely.start()
+      await pyrogram.idle()
+      zone = await get_datetime()
+      await bot.send_message(
+           chat_id=config.SUPPORT_CHAT,
+           text=strings.RESTART_TEXT1.format(date=zone["date"], time=zone["time"]))
+      await lovely.send_message(
+           chat_id=config.SUPPORT_CHAT,
+           text=strings.RESTART_TEXT2.format(date=zone["date"], time=zone["time"]))
+
+if __name__ == "__main__":
+    lovely.loop.run_until_complete(run_clients())
